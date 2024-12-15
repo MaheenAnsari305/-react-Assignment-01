e5a7b3416263d8bb472f2f174a85f150844af1bc
Maheen Ansari <muhammadyaseen8708@gmail.com>
2024-12-15 13:09:58 +0500

    Add files via upload

---
+++ React Assign 1/src/main.jsx
@@ -0,0 +1,19 @@
+import react from 'react'
+import ReactDom from 'react-dom/client'
+const main = document.getElementById('root')
+
+ReactDom.createRoot(main).render(
+
+<div style={{padding:"20px", border:"4px solid blue"} }>
+<h1 style={{color:"black , font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif" , fontSize:"50px" , textAlign:"center" , padding:"20px" , border:"4px solid red", fontStyle:"italic"}}>Well come to react.....</h1>
+<img style={{width:"200px" }} src="/public/vite.svg" alt="" />
+<p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat maiores repudiandae veniam, et porro cum vitae officia quae voluptates voluptatibus consequatur. Inventore vero facilis iure possimus maxime aut voluptatum impedit perferendis eius. Labore neque distinctio numquam iste ipsum quaerat nisi voluptas provident eos nulla, dignissimos vel culpa vero iusto maiores odio. Rem non blanditiis eos voluptatibus! Adipisci vitae quasi mollitia itaque. Iste dolore ab reprehenderit a necessitatibus. Ducimus, tempora in perspiciatis cum consequatur nesciunt harum delectus culpa ad atque porro quasi eveniet numquam enim explicabo iste accusantium molestiae. Quaerat minus temporibus corporis expedita? Sunt cupiditate excepturi quasi illo dolorum veritatis, repellendus, inventore sapiente cumque nisi consequatur cum doloremque quis, eligendi sit aut qui minus voluptatibus nobis. Perferendis itaque tempora nam. Cupiditate dolor sapiente cum iste necessitatibus voluptatibus, voluptate ipsam maxime sed saepe ipsum? Doloremque illum libero autem deleniti rem, delectus esse molestiae aliquid iste enim excepturi quasi sapiente voluptates sed praesentium in, accusantium nam aliquam corporis expedita veritatis est officia repellat voluptatibus. Corrupti doloribus nulla, praesentium atque necessitatibus voluptatem ipsum aut odit omnis architecto culpa. Perspiciatis cum aliquid magni ratione maiores facere, accusamus praesentium suscipit. Consequuntur temporibus cum officiis explicabo quas expedita illo, id at dolores officia, magnam nisi quasi!</p>
+<img style={{width:"200px"}} src="https://www.patterns.dev/img/reactjs/react-logo@3x.svg" alt="" />
+
+
+</div>
+
+
+
+
+)
